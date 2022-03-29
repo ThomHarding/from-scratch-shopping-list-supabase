acada122cce3280b2255f03fde38c7dea8556c6b
THOMAS HARDING <hardingt604@gmail.com>
2022-03-29 11:54:00 -0600

    filled out item form submit event listener

--- shopping-list/shopping-list.js
+++ shopping-list/shopping-list.js
@@ -1,13 +1,19 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, createItem, fetchAndDisplayList, logout } from '../fetch-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const itemForm = document.getElementById('shopping-list-form');
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
-//form submit event listener
-    //add item to supabase
-    //fetchanddisplayitems()
+itemForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let data = new FormData(itemForm);
+    let Item = data.get('Item');
+    await createItem(Item);
+    itemForm.reset();
+    fetchAndDisplayList();
+});
